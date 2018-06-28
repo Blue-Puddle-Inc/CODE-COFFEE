4e147db4f2895431ba0f5ced9530dbd4278c89d6
umadash <yamato@umadash.com>
2018-06-28 11:42:10 +0900

    JavaScriptのパッケージコードを追加

---
+++ JavaScript/Coffee.js
@@ -0,0 +1,9 @@
+export default class Coffee {
+  taste() {
+    return 'Delicious';
+  }
+
+  constructor() {
+
+  }
+}
---
+++ JavaScript/main.js
@@ -0,0 +1,4 @@
+import Coffee from './Coffee';
+
+const coffee = new Coffee();
+console.log(coffee.taste()); // Delicious
